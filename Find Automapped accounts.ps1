c795180cad550c69595168e670f237501e726688
hi2piyush <hi2piyush@gmail.com>
2024-06-25 20:53:14 +0530

    Find Automapped accounts.ps1

--- Find Automapped accounts.ps1
+++ Find Automapped accounts.ps1
@@ -1,3 +1,3 @@
 (Get-Mailboxpermission "SharedMailbox" -ReadFromDomainController)[0].DelegateListLink
 
-#Above will give Output as the accounts which are automapped
+#Above will give Output as only the accounts which are automapped
